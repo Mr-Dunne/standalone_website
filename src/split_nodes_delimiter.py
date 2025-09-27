5abb2aa085481b36037b3e39a72b5571ae18dcb1
shane_dunne <dunneshanem@gmail.com>
2025-09-27 01:57:02 -0400

    4) removed erronious print statements

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -22,8 +22,3 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(segment, text_type))
     
     return new_nodes
-
-node = TextNode("This is text with a `code block` word", TextType.TEXT)
-new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-
-print(new_nodes)
